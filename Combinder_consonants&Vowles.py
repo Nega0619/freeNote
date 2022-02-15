c3d7c843a9bde86580fe45ca705dc3722805c6f6
Nega0619 <h0riya0619@gmail.com>
2022-02-15 14:11:40 +0900

    ㅁㅇㅇ

--- Combinder_consonants&Vowles.py
+++ Combinder_consonants&Vowles.py
@@ -2,13 +2,13 @@
 import os
 import unicodedata
 
-file_path=r"D:\Github\Aiffel_nodes"
+file_path=r"D:\GitHub\Aiffel_nodes_fd_1-20"
 file_list = os.listdir(file_path)
 
 for f in file_list:
     if(f.endswith('.ipynb')):
         # print(f)
-        # print(type(f))
+        # print(type(f))D:\GitHub\Aiffel_nodes_fd_1-20
         src=os.path.join(file_path,f)
         uni1 = unicodedata.normalize('NFD', f)
         # print(uni1)
--- add_hyphen_each_line.py
+++ add_hyphen_each_line.py
@@ -6,16 +6,10 @@ lms에서 문자열을 복사한 후 주피터 노트북에 넣으면 `- `이거
 """
 
 multi_string = """
-(1) 입력 데이터: Real Image 역할을 할 images 한 세트를 입력으로 받음
-(2) 생성자 입력 노이즈 : generator가 FAKE IMAGE를 생성하기 위한 noise를 images 한 세트와 같은 크기인 BATCH_SIZE 만큼 생성함
-(3) tf.GradientTape()는 가중치 갱신을 위한 Gradient를 자동 미분으로 계산하기 위해 with 구문 열기
-(4) generated_images 생성 : generator가 noise를 입력받은 후 generated_images 생성
-(5) discriminator 판별 : discriminator가 Real Image인 images와 Fake Image인 generated_images를 각각 입력받은 후 real_output, fake_output 출력
-(6) loss 계산 : fake_output, real_output으로 generator와 discriminator 각각의 loss 계산
-(7) accuracy 계산 : fake_output, real_output으로 discriminator의 정확도 계산
-(8) gradient 계산 : gen_tape와 disc_tape를 활용해 gradient를 자동으로 계산
-(9) 모델 학습 : 계산된 gradient를 optimizer에 입력해 가중치 갱신
-(10) 리턴값 : 이번 스텝에 계산된 loss와 accuracy를 리턴
+칼럼 종류: Date, Open, High, Low, Close, Adj Close, Volume
+날짜별로 시작가(Open), 최고가(High), 최저가(Low), 종가(Close), 수정종가(Adj Close), 거래량(Volume)이 저장되어 있는 것을 확인할 수 있습니다.
+우리는 종가(Close)만 사용하겠습니다. (주식 시장은 정해진 시간 동안만 거래가 가능합니다. 종가란 하루의 장이 마감하였을 때의 가격입니다.)
+
 """
 
 splited_str = multi_string.split('\n')
@@ -29,5 +23,5 @@ for str in (splited_str[1:-1]):
     print('- '+str+'\n',end="")
     tobe_copy+='- '+str+'\n'
 
-clipboard.copy(tobe_copy)
+clipboard.copy(tobe_copy+'\n')
 # print(multi_string)
