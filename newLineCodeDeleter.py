1d046ae520b7a510a8427ac57d684b770f4a75bb
Nega0619 <h0riya0619@gmail.com>
2022-03-06 22:51:23 +0900

    .

---
+++ newLineCodeDeleter.py
@@ -0,0 +1,26 @@
+# pdf를 복붙하면 생기는 줄바꿈 문자를 다 삭제하는 것
+
+import clipboard
+
+# get texts
+f = open('text', 'r', encoding='UTF8')
+
+result = ""
+for line in f:
+    # print(line)
+    # print(line[-3:],',,')
+    # print(ord(line[-1:]))
+    # if(ord(line[-1:])==10):
+    #     result+=" " + line[:-1]
+    # else:
+    #     result+=line
+    if(line.endswith('.\n')):
+        line = line + '\n\n'
+
+    else:
+        line = line[:-1] + " "
+    result+=line
+
+print(result)
+
+clipboard.copy(result)
