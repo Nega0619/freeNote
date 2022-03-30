468d8ea0323e5644e3d171f32c52ae5cc30bedc5
Nega0619 <h0riya0619@gmail.com>
2022-03-30 23:29:48 +0900

    d

--- add_hyphen_each_line.py
+++ add_hyphen_each_line.py
@@ -6,13 +6,9 @@ lms에서 문자열을 복사한 후 주피터 노트북에 넣으면 `- `이거
 """
 
 multi_string = """
-데이터의 중복 제거
-NaN 결측치 제거
-한국어 토크나이저로 토큰화
-불용어(Stopwords) 제거
-사전word_to_index 구성
-텍스트 스트링을 사전 인덱스 스트링으로 변환
-X_train, y_train, X_test, y_test, word_to_index 리턴
+언어 모델이 발전해 온 과정을 개략적으로 파악한다.
+기존 RNN 기법이 번역에서 보인 한계를 파악하고, 이를 개선한 Seq2seq를 이해한다.
+Seq2seq를 발전시킨 Attention에 대해 알아본다.
 """
 
 
