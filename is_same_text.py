b919dd9b285cd93d9119514ae439d7df670eb06e
Nega0619 <80737049+Nega0619@users.noreply.github.com>
2022-07-26 11:51:13 +0900

    Create is_same_text.py

---
+++ is_same_text.py
@@ -0,0 +1,12 @@
+text1='''
+yeah expect people read actually accept hard atheism need little leap faith jimmy your logic runs steam sorry pity sorry that have these feelings denial about faith need well just pretend that will happily ever after anyway maybe start newsgroup atheist hard bummin much forget your flintstone chewables bake timmons
+'''
+
+text2='''
+yeah expect people read actually accept hard atheism need little leap faith jimmy your logic runs steam sorry pity sorry that have these feelings denial about faith need well just pretend that will happily ever after anyway maybe start newsgroup atheist hard bummin much forget your flintstone chewables bake timmons
+'''
+
+if text1 == text2:
+    print('same')
+else:
+    print('not same')
